c521fb1c62ea5b449efb0df65f93e1e705382928
Valery Chernov <valery.chernov@deelvin.com>
2023-05-12 09:59:59 +0300

    add tests for new kernel

--- tests/test_linear_kernels.py
+++ tests/test_linear_kernels.py
@@ -1,5 +1,12 @@
 import torch
-from torch_int._CUDA import linear_a8_w8_b32_o32, linear_relu_a8_w8_b8_o8, linear_a8_w8_b8_o8, linear_a8_w8_b32_o32_with_scaling, linear_a8_w8_bfp32_ofp32
+from torch_int._CUDA import (
+    linear_a8_w8_b32_o32,
+    linear_relu_a8_w8_b8_o8,
+    linear_relu_a8_w8_bfp32_ofp32,
+    linear_a8_w8_b8_o8,
+    linear_a8_w8_b32_o32_with_scaling,
+    linear_a8_w8_bfp32_ofp32,
+)
 from icecream import ic
 
 
@@ -85,6 +92,22 @@ def test_quant_linear_relu_a8_w8_b8_o8():
     ic(torch.allclose(y_gt.float(), y.float().cpu(), atol=1))
 
 
+@torch.no_grad()
+def test_quant_linear_relu_a8_w8_bfp32_ofp32():
+    B, M, N = 128, 512, 1024
+    weight = torch.randint(-128, 127, (N, M), dtype=torch.int8)
+    bias = torch.randn(N, dtype=torch.float32)
+    x = torch.randint(-128, 127, (B, M), dtype=torch.int8)
+    alpha, beta = 0.001, 1
+    linear = torch.nn.Linear(M, N, bias=True)
+    linear.weight.data = weight.float() * alpha
+    linear.bias.data = bias.float() * beta
+    y_gt = linear(x.float())
+    y = linear_relu_a8_w8_bfp32_ofp32(x.cuda(), weight.cuda(),
+                                bias.cuda(), alpha, beta)
+    ic(torch.allclose(y_gt, y.cpu(), atol=0.5))
+
+
 if __name__ == '__main__':
     print('test_quant_linear_a8_w8_b32_o32')
     test_quant_linear_a8_w8_b32_o32()
@@ -96,3 +119,5 @@ if __name__ == '__main__':
     test_quant_linear_a8_w8_b8_o8()
     print('test_quant_linear_relu_a8_w8_b8_o8')
     test_quant_linear_relu_a8_w8_b8_o8()
+    print('test_quant_linear_relu_a8_w8_bfp32_ofp32')
+    test_quant_linear_relu_a8_w8_bfp32_ofp32()
--- tests/test_linear_modules.py
+++ tests/test_linear_modules.py
@@ -1,5 +1,11 @@
 import torch
-from torch_int.nn.linear import W8A8B8O8LinearReLU, W8A8B8O8Linear, W8A8B32O32Linear, W8A8BFP32OFP32Linear
+from torch_int.nn.linear import (
+    W8A8B8O8LinearReLU,
+    W8A8BFP32OFP32LinearReLU,
+    W8A8B8O8Linear,
+    W8A8B32O32Linear,
+    W8A8BFP32OFP32Linear,
+)
 from icecream import ic
 
 
@@ -15,6 +21,20 @@ def test_w8a8b8o8_linear_relu():
     q_linear = W8A8B8O8LinearReLU.from_float(linear, x_scale, y_scale).cuda()
     q_y = q_linear(qx.cuda()).cpu()
     y_hat = q_y * y_scale
+    r2 = (y_gt - y_hat).pow(2).mean() / y_gt.pow(2).mean()
+    ic(r2)
+
+
+@torch.no_grad()
+def test_w8a8bfp32ofp32_linear_relu():
+    B, M, N = 128, 512, 1024
+    x = torch.randn(B, M)
+    x_scale = x.abs().max() / 127
+    qx = (x / x_scale).round().to(torch.int8)
+    linear = torch.nn.Linear(M, N, bias=True)
+    y_gt = linear(x).clamp(min=0)
+    q_linear = W8A8BFP32OFP32LinearReLU.from_float(linear, x_scale).cuda()
+    y_hat = q_linear(qx.cuda()).cpu()
     r2 = (y_gt - y_hat).pow(2).mean() / y_gt.pow(2).mean()
     ic(r2)
 
@@ -67,6 +87,8 @@ def test_w8a8bfp32ofp32_linear():
 if __name__ == '__main__':
     print('test_w8a8b8o8_linear_relu')
     test_w8a8b8o8_linear_relu()
+    print('test_w8a8bfp32ofp32_linear_relu')
+    test_w8a8bfp32ofp32_linear_relu()
     print('test_w8a8b8o8_linear')
     test_w8a8b8o8_linear()
     print('test_w8a8b32o32_linear_with_scaling')
